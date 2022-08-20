d9272d34701a85a8a8318e7e0c8d6ae8d71f86ec
Celine <108868474+SICelineChan@users.noreply.github.com>
2022-08-20 10:10:30 +0000

    Added a new FizzBuzz file

--- index.js
+++ index.js
@@ -3,28 +3,29 @@ w = w.toUpperCase();
 let d = "deposit";
 d = d.toUpperCase();
 
-var userKonto = 50; 
+var userKonto = 50;
 
 let firstReq = prompt("Press W to WITHDRAW or D to DEPOSIT.");
 if (firstReq === "w") {
   let howMuch = prompt("How much to withdraw?");
-    if (howMuch % 10 !== 0) {
+  if (howMuch % 10 !== 0) {
     console.log("Only enter number in x10 is given.");
   } else if (userKonto > howMuch) {
-    var newSum = (userKonto - (howMuch)); 
+    var newSum = (userKonto - (howMuch));
     console.log("You have " + (userKonto - (howMuch)) + " left in your account.");
     console.log(newSum);
-    
-    while ( noMore = 0 ) { 
-    var takeMore = prompt("Enter number to withdraw more fund.");
-    var evenMore = (newSum - takeMore);
-    console.log("You have €" + (newSum - takeMore) + " left.");
-    var noMore = (evenMore - takeMore);
-    console.log("You have reached your withdrawal limit!"); } 
+    d
+    while (noMore = 0) {
+      var takeMore = prompt("Enter number to withdraw more fund.");
+      var evenMore = (newSum - takeMore);
+      console.log("You have €" + (newSum - takeMore) + " left.");
+      var noMore = (evenMore - takeMore);
+      console.log("You have reached your withdrawal limit!");
+    }
   } else if (userKonto <= howMuch) {
     console.log("insufficient fund.");
   }
-} 
+}
 else if (firstReq === "d") {
   console.log("To deposit, please wait...");
   var addFund = parseInt(prompt("Enter deposit amount?"));
