8d6aa601d8197fcdfa5771e647b9bff095313497
Celine Chan <cctinlone@gmail.com>
2022-09-20 19:46:54 +0200

    exercise 9 in javescript

---
+++ javascript/JS_HWK/Hwk 2/exercise 9/index.js
@@ -0,0 +1,11 @@
+// Write a program that prints 1 per line only the names on odd positions in an array of names.
+
+let names = ["Josie", "Cathrin", "Bob", "Max", "Hannah", "Alex"];
+
+console.log(names[1]);
+
+for (let i = 0; i < names.length; i++) {
+  if (i % 2 !== 0) {
+    console.log(names[i]);
+  }
+}
