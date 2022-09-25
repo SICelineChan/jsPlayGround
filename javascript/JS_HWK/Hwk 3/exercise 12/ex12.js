4494674be523bb5cca548983c1fb2b43d9b9380a
Celine Chan <cctinlone@gmail.com>
2022-09-25 11:43:25 +0200

    exercise 12 completed

---
+++ javascript/JS_HWK/Hwk 3/exercise 12/ex12.js
@@ -0,0 +1,23 @@
+const userNameReverse = document.querySelector("#userNameReverse");
+function reverseString(str) {
+  // Step 1. Use the split() method to return a new array
+  var splitString = str.split(""); // var splitString = "hello".split("");
+  // ["h", "e", "l", "l", "o"]
+
+  // Step 2. Use the reverse() method to reverse the new created array
+  var reverseArray = splitString.reverse();
+  // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+  // ["o", "l", "l", "e", "h"]
+
+  // Step 3. Use the join() method to join all elements of the array into a string
+  var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+  // "olleh"
+
+  //Step 4. Return the reversed string
+  return joinArray; // "olleh"
+}
+
+let userName = reverseString(prompt("Enter your name here!"));
+console.log(userName);
+
+userNameReverse.innerHTML = `Here is your name in reverse will become: ${userName}`;
