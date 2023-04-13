cd5d3285ce5d7206af7e2a438fa0f5d7184b9643
Celine Chan <cctinlone@gmail.com>
2023-04-13 21:06:30 +0200

    counting some sheeps

---
+++ javascript/Function_Practice/countingSheeps.js
@@ -0,0 +1,21 @@
+let arrayOfSheep = [true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true ];
+
+console.log(arrayOfSheep.length);
+
+function countSheeps(arrayOfSheep) {
+    const isSheep = arrayOfSheep.filter(sheep => sheep);
+    console.log(isSheep);
+    return console.log(isSheep.length);  
+  }
+  countSheeps(arrayOfSheep);
+
+  let alsoSheep =  arrayOfSheep.filter(function(sheep) {
+    return sheep === true;
+  });
+  
+  console.log(`Also shows ${alsoSheep.length}`);
